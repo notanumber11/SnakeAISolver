206a8eac1c7d32c2e6c8398bbbc2cb4ba190bd79
notanumber11 <denisromasanta@gmail.com>
2020-05-05 00:23:48 +0200

    Fix to deploy easily to aws

--- snake/utils/aws_snake_utils.py
+++ snake/utils/aws_snake_utils.py
@@ -5,6 +5,7 @@ import json
 def _is_aws() -> bool:
     running_in_aws = True
     operative_system = os.getenv('OS')
+    print("The env OS is: {}".format(operative_system))
     if operative_system is not None and "windows" in operative_system.lower():
         running_in_aws = False
     print("Running in aws={}".format(running_in_aws))
@@ -18,24 +19,23 @@ def _is_aws() -> bool:
 
 
 def _get_hyperparameters(path: str) -> str:
+    print("The hyperparameters read from {} are:".format(path))
     with open(path) as f:
-        print("********************************")
-        print("The hyperparameters read from {} are:".format(path))
         data = json.load(f)
-        print(data)
-        print("********************************")
-        return data
-
+    print("Hyperparameters: {}".format(data))
+    return data
 
 def get_hyperparameters():
-    if _is_aws():
-        return _get_hyperparameters("/opt/ml/input/config/hyperparameters.json")
-    else:
-        return _get_hyperparameters("hyperparameters.json")
-
+    path = "/opt/ml/input/config/hyperparameters.json" if _is_aws() else "hyperparameters.json"
+    try:
+        return _get_hyperparameters(path)
+    except:
+        # Use the defaults provided in train_basic_genetic_algorithm
+        print("Could not read file from path: {}".format(path))
+        return {}
 
 def get_training_basic_genetic_output_folder() -> str:
     if _is_aws():
         return "/opt/ml/game/"
     else:
-        return "C:\\Users\\Denis\\Desktop\\SnakePython\\data\\basic_genetic\\"
+        return "..\\data\\basic_genetic\\"
